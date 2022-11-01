9d9c591c2380a85c07d2eba6fa35c5c3d288f044
Your Name <you@example.com>
2022-10-31 21:31:14 -0500

    miprimer git

--- src/componentes/general/Header.js
+++ src/componentes/general/Header.js
@@ -3,7 +3,7 @@ const Header = () =>{
         <div className="contai ner">
             <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
                 <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-                    <span className="fs-4">Doctor Online</span>
+                    <span className="fs-4">Tu Doctor Online</span>
                 </a>
 
             <ul className="nav nav-pills">
